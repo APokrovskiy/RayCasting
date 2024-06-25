770e3ec5adbef4749b46b1e7092473a18b444268
retrumer <retrumkill@gmail.com>
2024-06-25 16:10:43 +0500

    Создание коллайдеров мира при его инициализации

--- headers/World.hpp
+++ headers/World.hpp
@@ -4,17 +4,28 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include "ray-cast.hpp"
-
-
+#include "Collider.hpp"
 
 class World
 {
 public:
     using World_Map = std::set<rc::Coords>;
 
-    World(World_Map& wm, unsigned int tile): wm(wm), tile(tile){}
-    World(std::vector<std::string> wm, char wall,unsigned int tile): wm(rc::init_world_map(wm, wall)), tile(tile){}
-
+    World(World_Map& wm, unsigned int tile): wm(wm), tile(tile)
+    {
+        for(auto crd: wm)
+        {
+            wall_colliders.push_back(Collider({crd.x * tile,crd.y * tile},{(crd.x+1) * tile,(crd.y+1) * tile}));
+        }
+    }
+    World(std::vector<std::string> wm, char wall,unsigned int tile): wm(rc::init_world_map(wm, wall)), tile(tile)
+    {
+        for(auto crd: this->wm)
+        {
+            wall_colliders.push_back(Collider({crd.x * tile,crd.y * tile},{(crd.x+1) * tile,(crd.y+1) * tile}));
+        }
+    }
+    const std::vector<Collider>& get_colliders(){return wall_colliders;}
     const World_Map& get_walls_coords(){return wm;}
     unsigned int get_tile_size(){return tile;}
     
@@ -30,6 +41,7 @@ public:
     }
 
 private:
+    std::vector<Collider> wall_colliders;
     World_Map wm;
     unsigned int tile;
 };
