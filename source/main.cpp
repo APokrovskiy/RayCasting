37e26b42b73c46460f4951224898783db237a23c
retrumer <retrumkill@gmail.com>
2024-07-03 20:06:29 +0500

    Сделано отображение фпс

--- source/main.cpp
+++ source/main.cpp
@@ -3,17 +3,15 @@
 #include <vector>
 #include <set>
 #include <cmath>
-#include <thread>  
-#include <atomic>  
+#include <thread>
+#include <atomic>
 #include <fstream>
 #include <filesystem>
 
 #include <SFML/Graphics.hpp>
 #include <nlohmann/json.hpp>
 
-
 #include "World.hpp"
-
 
 #include "Camera.hpp"
 #include "Button.hpp"
@@ -23,13 +21,7 @@
 
 using json = nlohmann::json;
 
-
-
-
-
-
-
-void upload_settings(const ray_casting_settings& settings, Camera& cmr)
+void upload_settings(const ray_casting_settings &settings, Camera &cmr)
 {
     cmr.set_position(settings.cmr.cmr_pos_x, settings.cmr.cmr_pos_y);
     cmr.set_field_of_view(settings.cmr.fov);
@@ -39,16 +31,14 @@ void upload_settings(const ray_casting_settings& settings, Camera& cmr)
     cmr.set_visual_range(settings.cmr.vis_r);
 }
 
-
-//TODO: Обновить список хедеров
+// TODO: Обновить список хедеров
 
 // main.cpp
-///////////////////////////////////////////////////////////////////////////////////////////////// 
+/////////////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
-    std::atomic_bool is_configurator_opened {false};
+    std::atomic_bool is_configurator_opened{false};
 
-    
     std::string title = "Ray-Casting";
     std::string settings_file_path = "settings.json";
 
@@ -68,38 +58,40 @@ int main()
     settings_file.close();
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
     Settings_Updater setts_updater{settings_file_path};
 
     ray_casting_settings settings = setts_updater.get_settings();
     // Обновление настроек
-    
-
-
-
-
 
     // Инициализация объектов
     sf::RenderWindow window{{settings.win.screen_width, settings.win.screen_height}, title, sf::Style::Close | sf::Style::Titlebar};
 
     World world{settings.world.string_map,
-            settings.world.wall_char,
-            settings.world.tile_size};
+                settings.world.wall_char,
+                settings.world.tile_size};
 
     Camera cmr{world, 50};
 
     Background background{settings}; // TODO: Убрать зависимость от всей структуры настроек
 
-    Button menu_button {"../Media/GUI/ButtonsIcons/MenuButton.png"}; 
-    menu_button.set_scale({0.45,0.45});
+    Button menu_button{"../Media/GUI/ButtonsIcons/MenuButton.png"};
+    menu_button.set_scale({0.45, 0.45});
     int menu_button_shift = 12; // Смещение кнопки от границ окон
-    menu_button.set_position({settings.win.screen_width - menu_button.get_texture().getSize().x * menu_button.get_scale().x - menu_button_shift, menu_button_shift });
+    menu_button.set_position({settings.win.screen_width - menu_button.get_texture().getSize().x * menu_button.get_scale().x - menu_button_shift, menu_button_shift});
+    //загрузка шрифта
+    sf::Font font;
+    font.loadFromFile("../Media/GUI/Fonts/Ebbe.ttf");
 
+    int fps;
+    //создание текста для отображения фпс
+    sf::Text fpslabel{L"fps: ", font, 30};
+    fpslabel.setColor(sf::Color::Red);
+    fpslabel.setPosition({100, 100});
+
+    sf::Clock clock;
 
     // Загрузка настроек в камеру
     upload_settings(settings, cmr);
-    
-
 
     // Главный цикл
     while (window.isOpen())
@@ -112,13 +104,19 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            else if(event.type == sf::Event::MouseButtonReleased && menu_button.isClicked(window,event.mouseButton))
-                    // Создание потока с конфигуратором
-                    start_configurator(is_configurator_opened);
-
+            else if (event.type == sf::Event::MouseButtonReleased && menu_button.isClicked(window, event.mouseButton))
+                // Создание потока с конфигуратором
+                start_configurator(is_configurator_opened);
+        }
+        //расчет фпс
+        if (clock.getElapsedTime().asSeconds() >= 1)
+        {
+            clock.restart();
+            fpslabel.setString("fps: " + std::to_string(fps));
+            fps = 0;
         }
 
-        if(window.hasFocus())
+        if (window.hasFocus())
             cmr.move();
 
         window.clear();
@@ -128,9 +126,11 @@ int main()
         cmr.draw(window, Camera::Rendering_Mode::M_3D);
 
         menu_button.draw(window);
-        
+        window.draw(fpslabel);
         window.display();
+
+        fps++;
     }
-    
+
     return 0;
 }
