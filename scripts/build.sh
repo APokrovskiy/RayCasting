4e239dcf725e02faf77f7ba23828f91206fca0db
APokrovskiy <pokrantnicol@gmail.com>
2024-07-07 13:59:24 +0500

    Удалены скрипты build

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -Wno-dev -DCMAKE_CXX_COMPILER=c++ -DCMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++" -G Ninja -S .. -B ../build && cmake --build ../build 
-read -rp "Нажмите Enter, чтобы продолжить..."
